94cae7e413f39fd45700ce5bebe090af7ea4da45
omerfkzr <omer.kazar22@gmail.com>
2023-04-08 11:41:42 +0300

    Add Project Files
    Change the Folder Hierarchy to MVVM

---
+++ MovieBook/MovieBook/App/MainStruct/MovieBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookApp.swift
+//  MovieBook
+//
+//  Created by Ömer Faruk Kazar on 8.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
